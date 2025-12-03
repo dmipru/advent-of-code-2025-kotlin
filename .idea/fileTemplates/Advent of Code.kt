0067602fd0538d87f3e2069d498a855b1b746c19
Dmitrii Prutckov <dmitrii.prutckov@gmail.com>
2025-12-03 19:06:52 +0000

    Update AOC File template

--- .idea/fileTemplates/Advent of Code.kt
+++ .idea/fileTemplates/Advent of Code.kt
@@ -1,16 +1,18 @@
 #set( $Code = "bar" )
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    check(part1(listOf("...")) == 1)
+    val testInput = readInput("Day${Day}_test")
+    check(part1(testInput).also { println("part 1 test actual: \$it") } == 1) { "part 1 failed" }
+    check(part2(testInput).also { println("part 2 test actual: \$it") } == 1) { "part 2 failed" }
 
     val input = readInput("Day$Day")
-    part1(input).println()
-    part2(input).println()
+    part1(input).println("part 1 answer: ")
+    part2(input).println("part 2 answer: ")
+}
+
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+private fun part2(input: List<String>): Int {
+    return input.size
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,6 +19,7 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+fun Any?.println(prefix: String) = "${prefix}${this}".println()
 
 fun Long.pow(power: Int): Long {
     if (power == 0) return 1
